086e129e77893028acf2d6cfc9204cb0d0592559
slampick <paidulov-job@yandex.ru>
2015-05-15 00:53:05 +0400

    Added new radio station

--- app/src/main/java/com/example/radioadek/activity/FirstActivity.java
+++ app/src/main/java/com/example/radioadek/activity/FirstActivity.java
@@ -1,6 +1,5 @@
 package com.example.radioadek.activity;
 
-import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -32,12 +31,38 @@ public class FirstActivity extends ActionBarActivity {
         ListView list = (ListView) findViewById(R.id.radio_list);
 
         ArrayList<Radio> radios = new ArrayList<>();
-        for(int i = 0; i < 5; i++) {
-            Radio radio = new Radio("Radio Record", "Trance, Electronic",
-                    BitmapFactory.decodeResource(getResources(), R.drawable.record),
-                    "http://cast.loungefm.com.ua/terrace128");
-            radios.add(radio);
-        }
+
+        radios.add(new Radio("Radio Record", "Station, Deep, Breaks, Dancecore, Dubstep, Trap," +
+                "Hardstyle, Rock",
+                BitmapFactory.decodeResource(getResources(), R.drawable.record),
+                "http://online.radiorecord.ru:8101/rr_128"));
+        radios.add(new Radio("Biker-FM", "Rock",
+                BitmapFactory.decodeResource(getResources(), R.drawable.biker_fm),
+                "http://stream1.radiostyle.ru:8001/biker-fm"));
+        radios.add(new Radio("SKY.FM Roots Reggae", "Регги",
+                BitmapFactory.decodeResource(getResources(), R.drawable.sky_fm),
+                "http://radio02-cn03.akadostream.ru:8104/reggae_1_sky96.mp3"));
+        radios.add(new Radio("Bluese", "Bluese",
+                BitmapFactory.decodeResource(getResources(), R.drawable.bluese),
+                "http://music.myradio.com.ua:8000/blues128.mp3"));
+        radios.add(new Radio("Dance Club", "Dance, Electrinic, HIP-HOP",
+                BitmapFactory.decodeResource(getResources(), R.drawable.dance_club),
+                "http://music.myradio.com.ua:8000/dance128.mp3"));
+        radios.add(new Radio("Дорожное радио", "POP",
+                BitmapFactory.decodeResource(getResources(), R.drawable.street_radio),
+                "http://dorognoe.hostingradio.ru:8000/dorognoe"));
+        radios.add(new Radio("Юмор FM", "POP",
+                BitmapFactory.decodeResource(getResources(), R.drawable.humor),
+                "http://89.20.132.26:8000/v5_1"));
+        radios.add(new Radio("Европа ПЛЮС", "Station, Breaks, Deep, Dance Core, Dubstep, Trap, Hard",
+                BitmapFactory.decodeResource(getResources(), R.drawable.evrope_plus),
+                "http://onair.eltel.net/europaplus-128k"));
+        radios.add(new Radio("Zaycev FM", "Pop, Disco, Club, RnB",
+                BitmapFactory.decodeResource(getResources(), R.drawable.zaycev),
+                "http://zaycev.fm:9002/zaycev(128)"));
+        radios.add(new Radio("Шансон FM", "Шансон",
+                BitmapFactory.decodeResource(getResources(), R.drawable.shanson),
+                "http://music.myradio.com.ua:8000/shanson128.mp3"));
 
         adapter = new RadioAdapter(this.getApplicationContext(), radios);
         list.setAdapter(adapter);
